7b797c32d102f99b0920f45589a1282cc991883f
fafniir <127908979+fafniir@users.noreply.github.com>
2023-04-11 14:42:17 -0300

    Add files via upload

---
+++ EX01.py
@@ -0,0 +1,8 @@
+print('Gostaria de aprender python?')
+print('Se Sim, digite 1')
+print('Se Não, digite 0')
+yes = int(input())
+if yes == (1):
+    print('Muito bem, Vamos lá!!!')
+else:
+    print('então se lasca ai')
